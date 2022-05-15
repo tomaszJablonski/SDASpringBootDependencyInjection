b4df34df392401b90840975ca55df7a61de0297d
MrMag1990 <MrMag1990@kursSDA.pl>
2022-05-15 10:53:21 +0200

    Initial commit

---
+++ src/main/java/com/example/sdaspringbootdependencyinjection/SdaSpringBootDependencyInjectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.sdaspringbootdependencyinjection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdaSpringBootDependencyInjectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdaSpringBootDependencyInjectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sdaspringbootdependencyinjection/SdaSpringBootDependencyInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sdaspringbootdependencyinjection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdaSpringBootDependencyInjectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
